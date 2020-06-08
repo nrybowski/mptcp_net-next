b04d6cff8748c21366ec9ab15f96b8252c0fb212
Paolo Abeni <pabeni@redhat.com>
2020-06-08 23:51:31 +0200

    Squash-to: "net: mptcp: improve fallback to TCP"
    Drop socket lock on mptcp poll: we don't need it anymore,
    as we only use atomic ops on msk flags
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -1945,7 +1945,6 @@ static __poll_t mptcp_poll(struct file *file, struct socket *sock,
 
 	msk = mptcp_sk(sk);
 	sock_poll_wait(file, sock, wait);
-	lock_sock(sk);
 
 	if (test_bit(MPTCP_DATA_READY, &msk->flags))
 		mask = EPOLLIN | EPOLLRDNORM;
@@ -1954,8 +1953,6 @@ static __poll_t mptcp_poll(struct file *file, struct socket *sock,
 		mask |= EPOLLOUT | EPOLLWRNORM;
 	if (sk->sk_shutdown & RCV_SHUTDOWN)
 		mask |= EPOLLIN | EPOLLRDNORM | EPOLLRDHUP;
-
-	release_sock(sk);
 
 	return mask;
 }
